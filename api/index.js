1d0e6c8219f1c083c67facfb3223df732236c604
Isaac Ch <89037579+Txaverria@users.noreply.github.com>
2024-12-06 13:46:16 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
+// imports de los packages de node
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
+// config del app de Express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // no es obligatoriamente necesario siempre, pero es costumbre ponerlo.
 
-const port = process.env.PORT || 3000;
 
+// puerto y listen creados
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
