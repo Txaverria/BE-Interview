6c93d00b605b1818aa8a1663740471a1f30bc381
Isaac Ch <89037579+Txaverria@users.noreply.github.com>
2024-12-06 18:13:09 -0600

    Connect to MongoDB in index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 // imports de los packages de node
 const express = require("express");
-const app = express();
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 // routes
 const calculate = require("./routes/calculate");
 
+// crear el app para usar Express
+const app = express();
+
 // config del app de Express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // no es obligatoriamente necesario siempre, pero es costumbre ponerlo.
+
+// conectar a mongodb
+connectDB();
 
 // instrucción para usar las rutas
 app.use("/api", calculate);
