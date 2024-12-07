f3439deb9a2706d60b2f76d224a14cd9284a6d89
Isaac Ch <89037579+Txaverria@users.noreply.github.com>
2024-12-06 18:43:26 -0600

    Update Log.js

--- api/models/Log.js
+++ api/models/Log.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const LogSchema = new mongoose.Schema({
-  operacion: { type: String, required: true },
-  num1: { type: Number, required: true },
-  num2: { type: Number, required: true },
-  resultado: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now },
-  responseTime: { type: Number, required: true }
-});
+const LogSchema = new mongoose.Schema(
+  {
+    operacion: { type: String, required: true },
+    num1: { type: Number, required: true },
+    num2: { type: Number, required: true },
+    resultado: { type: Number, required: true },
+    timestamp: { type: Date, default: Date.now },
+    responseTime: { type: Number, required: true },
+  },
+  { collection: "Log" }
+);
 
 module.exports = mongoose.model("Log", LogSchema);
